b8bbaf5aa97064de42719a8fb09bf3e08fea4e48
LanceZPF <zpf4wp@outlook.com>
2022-02-10 23:16:49 -0500

    change fctrain

--- fctrain.py
+++ fctrain.py
@@ -1,8 +1,8 @@
 import argparse
 import copy
 import os
-os.environ['CUDA_VISIBLE_DEVICES']='0,1,2,3'
-# os.environ['CUDA_VISIBLE_DEVICES']='4,5,6,7'
+# os.environ['CUDA_VISIBLE_DEVICES']='0,1,2,3'
+os.environ['CUDA_VISIBLE_DEVICES']='4,5,6,7'
 import os.path as osp
 import time
 import warnings
